1219798ebaad43b1b4edc2f118b6b019868895b9
Yes-said <bobsaid358@gmail.com>
2024-04-30 15:10:55 +0300

    app and component files updated

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
         <nav className="flex justify-between items-center
         bg-slate-800 px-8 py-3">
             <Link className="text-white font-bold" href={"/"}>GTCoding.</Link>
-            <Link href={"/addTopic"}>Add Topic</Link>
+            <Link className="bg-white p-2" href={"/addTopic"}>Add Topic</Link>
         </nav>
     );
 }
